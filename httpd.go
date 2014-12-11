67ff756e8856d1a57bbcd7dae2ffaeb104a81871
Adrian Ratnapala <a.ratnapala@ngbits.com>
2014-12-11 08:38:45 +0100

    svg: introducing xmlWriter

--- httpd.go
+++ httpd.go
@@ -25,53 +25,100 @@ type Path struct {
 	vertices []vertex
 }
 
-type svgWriter struct {
-	open_tags []string
+type xmlWriter struct {
 	w io.Writer
+	lineStarted, longAttr, tagOpen bool
+	path []string
 }
 
-func (s *svgWriter) push(tag string) error {
-	s.open_tags = append(s.open_tags, tag)
-	_, err := fmt.Fprintf(s.w, "<%s>", tag)
-	return err
+func (x* xmlWriter) fmt(f string, args ...interface{}) {
+	if _, err := fmt.Fprintf(x.w, f, args...); err != nil {
+		panic(err)
+	}
+	x.lineStarted = true
 }
 
-func (s *svgWriter) pop() error {
-	tags := s.open_tags
+// writes a formatted string, but indents unless lineStarted
+func (x* xmlWriter) iFmt(f string, args ...interface{}) {
+	if !x.lineStarted {
+		w := x.w
+		for k := 0; k < len(x.path); k++ {
+			if _, err := io.WriteString(w, "    "); err != nil {
+				panic(err)
+			}
+		}
+	}
+	x.fmt(f, args...)
+}
 
+// Like iFmt except it writes only a single space if not right after NewLine.
+func (x* xmlWriter) wsFmt(f string, args ...interface{}) {
+	if !x.lineStarted {
+		x.iFmt(f, args...)
+		return
+	}
+
+	if _, err := io.WriteString(x.w, " "); err != nil {
+		panic(err)
+	}
+	x.fmt(f, args...)
+}
+
+
+// Emit "<tag" and be ready to accept attributes, or TagDone.
+func (x* xmlWriter) tagStart(tag string) {
+	x.iFmt("<%s", tag)
+	x.path = append(x.path, tag)
+	x.tagOpen = true
+}
+
+// Writes `key="value"`
+func (x* xmlWriter) attr(key string, value interface{}) {
+	x.wsFmt(`%s="%v"`, key, value)
+}
+
+// Adds a newline and keeps tracking information used by the indenter
+func (x* xmlWriter) newLine() {
+	x.longAttr = x.tagOpen
+	x.lineStarted = false
+	if _, err := io.WriteString(x.w, "\n"); err != nil {
+		panic(err);
+	}
+}
+
+func (x* xmlWriter) pop() string {
+	tags := x.path
 	n := len(tags) - 1
-	if n < 0  {
-		panic(fmt.Errorf("%v: too many pop()s"))
-	}
-
-	if _, err := fmt.Fprintf(s.w, "</%s>", tags[n]); err != nil {
-		return err
-	}
-
-	s.open_tags = tags[:n]
-
-	return nil
+	tag := tags[n]
+	x.path = tags[:n]
+	return tag
 }
 
-func (s *svgWriter) End() error {
-	tags := s.open_tags
-	//log.Printf("End: tags = %v", tags)
-
-	switch n := len(tags); {
-	case n <= 0: return nil
-	case n > 1: panic(fmt.Errorf(
-		"%v: unexpected End(), while %s is open.",
-		s, tags[n-1]))
+// Ends the current tag with `>`, unless pop, then end it with `/>`.
+func (x* xmlWriter) _tagEnd(txt string)  {
+	if x.longAttr {
+		x.newLine()
 	}
 
-	return s.pop();
+	x.tagOpen = false
+	x.iFmt(txt)
+	x.newLine()
 }
 
+// Ends the current tag with `>`.
+func (x* xmlWriter) tagEnd() {
+	x._tagEnd(">")
+}
 
-func newSvgWriter(w io.Writer) (*svgWriter, error) {
-	s := &svgWriter { w :w }
-	s.push("svg")
-	return s, nil
+// Closes the currently element with an end tag or `/>`.
+func (x* xmlWriter) tagPop() {
+	if x.tagOpen {
+		x._tagEnd("/>")
+		x.pop()
+		return
+	}
+	x.iFmt("</%s>", x.pop())
+	x.newLine()
 }
 
 
@@ -122,14 +169,35 @@ func svgHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	{
-		sw, err := newSvgWriter(os.Stdout)
-		if err != nil {
-			log.Fatal(err)
+	defer func() {
+		r := recover()
+		if r != nil {
+			log.Fatal(r)
 		}
-		sw.End()
-	}
+	}()
 
+	x := &xmlWriter{w : os.Stdout}
+	// <die ...>
+	x.tagStart("die")
+	x.attr("xmlns", "http://www.w3.org/2000/svg")
+	x.newLine()
+	x.attr("width", 1000)
+	x.attr("height", 1000)
+	x.tagEnd()
+	// <quck .../>
+	x.tagStart("quck")
+	x.attr("id", 14)
+	x.tagPop()
+	// <longer ...></longer>
+	x.tagStart("longer")
+	x.newLine()
+	x.attr("id", "tweedle-dee-is-dum")
+	x.tagPop()
+	// </die>
+	x.tagPop()
+}
+
+func Main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/savage/", svgHandler)
 	http.ListenAndServe(":8080", nil)
